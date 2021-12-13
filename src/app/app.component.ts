1bec84403e46eebdcc2c7db763255549319ee4ef
Jadir J. S. Junior <jadirjsjunior@gmail.com>
2021-12-13 11:45:17 -0300

    :construction: put icons on navegation and create minimal design to home/login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,16 @@ import { MatIconRegistry } from '@angular/material/icon'
   selector: 'app-root',
   template: `
     <mat-toolbar color="primary">
+      <button mat-icon-button><mat-icon>menu</mat-icon></button>
       <mat-icon svgIcon="lemon"></mat-icon>
       <a mat-button routerLink="/home"><h1>LemonMart</h1></a>
+      <span class="flex-spacer"></span>
+      <button mat-icon-button>
+        <mat-icon>account_circle</mat-icon>
+      </button>
+      <button mat-icon-button>
+        <mat-icon>lock_open</mat-icon>
+      </button>
     </mat-toolbar>
     <router-outlet></router-outlet>
   `,

4aef16dc09ce47749034e5146da3142de9b4dfc4
Jadir J. S. Junior <jadirjsjunior@gmail.com>
2021-12-13 13:24:24 -0300

    :construction: implemented test on home

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,25 +1,32 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { HomeComponent } from './home.component';
+import { By } from '@angular/platform-browser'
+import { HomeComponent } from './home.component'
 
 describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+  let component: HomeComponent
+  let fixture: ComponentFixture<HomeComponent>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
-  });
+      declarations: [HomeComponent],
+    }).compileComponents()
+  })
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(HomeComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  it('should create with title "Hello, Limoncu!" and a button with name "Login"', () => {
+    const titleEl: HTMLElement = fixture.debugElement.query(By.css('span')).nativeElement
+    const loginButtonEl: HTMLButtonElement = fixture.debugElement.query(
+      By.css('button')
+    ).nativeElement
+
+    expect(component).toBeTruthy()
+    expect(titleEl.textContent).toBe('Hello, Limoncu!')
+    expect(loginButtonEl.textContent).toBe('Login')
+  })
+})

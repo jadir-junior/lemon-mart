18018aa98c441eef382443d69735b1e9fc978ba6
Jadir J. S. Junior <jadirjsjunior@gmail.com>
2021-12-13 13:48:34 -0300

    :wrench: config manager home in router of manager

--- src/app/manager/manager-routing.module.ts
+++ src/app/manager/manager-routing.module.ts
@@ -1,8 +1,12 @@
 import { RouterModule, Routes } from '@angular/router'
 
+import { ManagerHomeComponent } from './manager-home/manager-home.component'
 import { NgModule } from '@angular/core'
 
-const routes: Routes = []
+const routes: Routes = [
+  { path: '', redirectTo: '/manager/home', pathMatch: 'full' },
+  { path: 'home', component: ManagerHomeComponent },
+]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

bb0f997da4f91efeaed5a216c778034a1b38a7a8
Jadir J. S. Junior <jadirjsjunior@gmail.com>
2021-12-14 09:10:41 -0300

    fix style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,9 @@ import { BrowserModule } from '@angular/platform-browser'
 import { FlexLayoutModule } from '@angular/flex-layout'
 import { HomeComponent } from './home/home.component'
 import { HttpClientModule } from '@angular/common/http'
-import { InventoryModule } from './inventory/inventory.module'
 import { MaterialModule } from './material.module'
 import { NgModule } from '@angular/core'
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
-import { PosModule } from './pos/pos.module'
-import { UserModule } from './user/user.module'
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, PageNotFoundComponent],
@@ -20,9 +17,6 @@ import { UserModule } from './user/user.module'
     BrowserAnimationsModule,
     MaterialModule,
     FlexLayoutModule,
-    InventoryModule,
-    PosModule,
-    UserModule,
     HttpClientModule,
   ],
   providers: [],
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,16 +1,12 @@
 import { CommonModule } from '@angular/common'
-import { NgModule } from '@angular/core'
-import { UserRoutingModule } from './user-routing.module';
-import { ProfileComponent } from './profile/profile.component';
-import { LogoutComponent } from './logout/logout.component';
+import { LogoutComponent } from './logout/logout.component'
 import { NavigationMenuComponent } from './navigation-menu/navigation-menu.component'
+import { NgModule } from '@angular/core'
+import { ProfileComponent } from './profile/profile.component'
+import { UserRoutingModule } from './user-routing.module'
 
 @NgModule({
-  declarations: [
-    ProfileComponent,
-    LogoutComponent,
-    NavigationMenuComponent
-  ],
+  declarations: [ProfileComponent, LogoutComponent, NavigationMenuComponent],
   imports: [CommonModule, UserRoutingModule],
 })
 export class UserModule {}

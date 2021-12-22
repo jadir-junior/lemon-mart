8233c629c294748d54bba62d8441085e48d8f897
Jadir J. S. Junior <jadirjsjunior@gmail.com>
2021-12-22 20:31:54 -0300

    create services auth
    create services auth
    add fkae inmemory auth on app module
    install jwt-decode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { AppComponent } from './app.component'
 import { AppRoutingModule } from './app-routing.module'
+import { AuthService } from './auth/auth.service'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { BrowserModule } from '@angular/platform-browser'
 import { FlexLayoutModule } from '@angular/flex-layout'
 import { HomeComponent } from './home/home.component'
 import { HttpClientModule } from '@angular/common/http'
+import { InMemoryAuthService } from './auth/auth.inmemory.service'
 import { MaterialModule } from './material.module'
 import { NgModule } from '@angular/core'
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
@@ -19,7 +21,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     FlexLayoutModule,
     HttpClientModule,
   ],
-  providers: [],
+  providers: [{ provide: AuthService, useClass: InMemoryAuthService }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}

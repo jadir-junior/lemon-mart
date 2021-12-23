08f66634289c4d900a419150632677a46bc81f2a
Jadir J. S. Junior <jadirjsjunior@gmail.com>
2021-12-23 12:52:52 -0300

    implementing login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { RouterModule, Routes } from '@angular/router'
 
 import { HomeComponent } from './home/home.component'
+import { LoginComponent } from './login/login.component'
 import { NgModule } from '@angular/core'
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'login/:redirectUrl', component: LoginComponent },
   {
     path: 'manager',
     loadChildren: () => import('./manager/manager.module').then((m) => m.ManagerModule),

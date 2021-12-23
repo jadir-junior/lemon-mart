aab4f71c089a6696e65c7d1e1532bba83f287b12
Jadir J. S. Junior <jadirjsjunior@gmail.com>
2021-12-23 13:15:23 -0300

    :construction:  logic navigation
    remove all buttons if user is not authenticated
    show a picture on account_circle if the user has a photo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './auth/auth.service'
 import { Component } from '@angular/core'
 import { DomSanitizer } from '@angular/platform-browser'
 import { MatIconRegistry } from '@angular/material/icon'
@@ -5,22 +6,38 @@ import { MatIconRegistry } from '@angular/material/icon'
 @Component({
   selector: 'app-root',
   template: `
-    <mat-toolbar color="primary" fxLayoutGap="8px">
-      <button mat-icon-button><mat-icon>menu</mat-icon></button>
+    <mat-toolbar
+      *ngIf="{
+        status: authService.authStatus$ | async,
+        user: authService.currentUser$ | async
+      } as auth"
+      color="primary"
+      fxLayoutGap="8px"
+    >
+      <button *ngIf="auth?.status?.isAuthenticated" mat-icon-button>
+        <mat-icon>menu</mat-icon>
+      </button>
       <a mat-button routerLink="/home">
         <mat-icon svgIcon="lemon"></mat-icon>
         <span class="mat-h2">LemonMart</span>
       </a>
       <span class="flex-spacer"></span>
       <button
+        *ngIf="auth?.status?.isAuthenticated"
         mat-mini-fab
         routerLink="/user/profile"
         matTooltip="Profile"
         aria-label="User Profile"
       >
-        <mat-icon>account_circle</mat-icon>
+        <img
+          *ngIf="auth?.user?.picture"
+          class="image-cropper"
+          [src]="auth?.user?.picture"
+        />
+        <mat-icon *ngIf="!auth?.user?.picture">account_circle</mat-icon>
       </button>
       <button
+        *ngIf="auth?.status?.isAuthenticated"
         mat-mini-fab
         routerLink="/user/logout"
         matTooltip="Logout"
@@ -31,9 +48,25 @@ import { MatIconRegistry } from '@angular/material/icon'
     </mat-toolbar>
     <router-outlet></router-outlet>
   `,
+  styles: [
+    `
+      .image-cropper {
+        width: 40px;
+        height: 40px;
+        position: relative;
+        overflow: hidden;
+        border-radius: 50%;
+        margin-top: -8px;
+      }
+    `,
+  ],
 })
 export class AppComponent {
-  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+  constructor(
+    iconRegistry: MatIconRegistry,
+    sanitizer: DomSanitizer,
+    public authService: AuthService
+  ) {
     iconRegistry.addSvgIcon(
       'lemon',
       sanitizer.bypassSecurityTrustResourceUrl('assets/img/icons/lemon.svg')

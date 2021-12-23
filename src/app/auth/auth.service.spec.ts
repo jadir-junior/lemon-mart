38b0a4f3fa78b8cf82c3c4c61a1f345403c2ef09
Jadir J. S. Junior <jadirjsjunior@gmail.com>
2021-12-23 10:29:43 -0300

    implement an inmemory authentication with a simple login

--- src/app/auth/auth.service.spec.ts
+++ src/app/auth/auth.service.spec.ts
@@ -1,16 +1,17 @@
-import { TestBed } from '@angular/core/testing';
-
-import { AuthService } from './auth.service';
+import { AuthService } from './auth.service'
+import { TestBed } from '@angular/core/testing'
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let service: AuthService
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthService);
-  });
+    TestBed.configureTestingModule({
+      providers: [AuthService],
+    })
+    service = TestBed.inject(AuthService)
+  })
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+    expect(service).toBeTruthy()
+  })
+})
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { combineLatest, filter, tap } from 'rxjs'
+
+import { AuthService } from '../auth/auth.service'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-home',
   template: `
     <div fxLayout="column" fxLayoutAlign="center center">
       <span class="mat-display-2">Hello, Limoncu!</span>
-      <button mat-raised-button color="primary">Login</button>
+      <button mat-raised-button color="primary" (click)="login()">
+        Login as Manager
+      </button>
     </div>
   `,
   styles: [
@@ -16,4 +22,19 @@ import { Component } from '@angular/core'
     `,
   ],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  login(): void {
+    this.authService.login('manager@test.com', '12345678')
+
+    combineLatest([this.authService.authStatus$, this.authService.currentUser$])
+      .pipe(
+        filter(([authStatus, user]) => authStatus.isAuthenticated && user?._id !== ''),
+        tap(([authStatus, user]) => {
+          this.router.navigate(['/manager'])
+        })
+      )
+      .subscribe()
+  }
+}

84d886e150497a7cf442b52eb15742dde7b11f20
Jadir J. S. Junior <jadirjsjunior@gmail.com>
2022-03-23 15:33:15 -0300

    fix test

--- src/app/auth/auth-custom.service.spec.ts
+++ src/app/auth/auth-custom.service.spec.ts
@@ -1,11 +1,14 @@
 import { AuthCustomService } from './auth-custom.service'
+import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { TestBed } from '@angular/core/testing'
 
 describe('AuthCustomService', () => {
   let service: AuthCustomService
 
   beforeEach(() => {
-    TestBed.configureTestingModule({})
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    })
     service = TestBed.inject(AuthCustomService)
   })
 

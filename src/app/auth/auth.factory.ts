f2888f95e29af4674cfd6bf0e439011832c0979e
Jadir J. S. Junior <jadirjsjunior@gmail.com>
2022-03-23 13:37:16 -0300

    :wrench: implemented custom server on auth factory

--- src/app/auth/auth.factory.ts
+++ src/app/auth/auth.factory.ts
@@ -1,16 +1,18 @@
 import { AngularFireAuth } from '@angular/fire/compat/auth'
+import { AuthCustomService } from './auth-custom.service'
 import { AuthMode } from './auth.enum'
 import { FirebaseAuthService } from './auth.firebase.service'
+import { HttpClient } from '@angular/common/http'
 import { InMemoryAuthService } from './auth.inmemory.service'
 import { environment } from 'src/environments/environment'
 
-export function authFactory(afAuth: AngularFireAuth) {
+export function authFactory(afAuth: AngularFireAuth, httpClient: HttpClient) {
   switch (environment.authMode) {
     case AuthMode.InMemory:
       return new InMemoryAuthService()
     case AuthMode.Firebase:
       return new FirebaseAuthService(afAuth)
     case AuthMode.CustomServer:
-      throw new Error('Not yet implemented')
+      throw new AuthCustomService(httpClient)
   }
 }

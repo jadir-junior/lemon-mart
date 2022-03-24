845ead2a55b71d5e200fda4321e0011f23ee6adf
Jadir J. S. Junior <jadirjsjunior@gmail.com>
2022-03-24 09:00:26 -0300

    :bug: fix test

--- src/app/user/user/user.service.spec.ts
+++ src/app/user/user/user.service.spec.ts
@@ -1,18 +1,23 @@
+import { AuthService } from 'src/app/auth/auth.service'
 import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { TestBed } from '@angular/core/testing'
 import { UserService } from './user.service'
 
 describe('UserService', () => {
   let service: UserService
+  let authService: AuthService
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
+      providers: [AuthService],
     })
     service = TestBed.inject(UserService)
+    authService = TestBed.inject(AuthService)
   })
 
   it('should be created', () => {
     expect(service).toBeTruthy()
+    expect(authService).toBeTruthy()
   })
 })

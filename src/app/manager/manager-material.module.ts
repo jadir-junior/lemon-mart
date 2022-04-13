6c9c8d8a76d877493c3d512d0e09bb7d537cd6b3
Jadir J. S. Junior <jadirjsjunior@gmail.com>
2022-04-13 13:40:47 -0300

    create angular material module on manager

---
+++ src/app/manager/manager-material.module.ts
@@ -0,0 +1,18 @@
+import { MatPaginatorModule } from '@angular/material/paginator'
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
+import { MatSlideToggleModule } from '@angular/material/slide-toggle'
+import { MatSortModule } from '@angular/material/sort'
+import { MatTableModule } from '@angular/material/table'
+import { NgModule } from '@angular/core'
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatSlideToggleModule,
+    MatSortModule,
+    MatTableModule,
+  ],
+})
+export class ManagerMaterialModule {}

0316edc099e527f86445cf64935f0026d7cc5483
Jadir J. S. Junior <jadirjsjunior@gmail.com>
2022-04-13 15:40:34 -0300

    fix testings

--- src/app/manager/user-table/user-table.component.spec.ts
+++ src/app/manager/user-table/user-table.component.spec.ts
@@ -1,6 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 
+import { AuthService } from 'src/app/auth/auth.service'
+import { HttpClientTestingModule } from '@angular/common/http/testing'
+import { ManagerMaterialModule } from '../manager-material.module'
+import { NO_ERRORS_SCHEMA } from '@angular/core'
+import { NoopAnimationsModule } from '@angular/platform-browser/animations'
+import { User } from 'src/app/user/user/user'
 import { UserTableComponent } from './user-table.component'
+import { of } from 'rxjs'
 
 describe('UserTableComponent', () => {
   let component: UserTableComponent
@@ -9,12 +16,17 @@ describe('UserTableComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [UserTableComponent],
+      imports: [HttpClientTestingModule, ManagerMaterialModule, NoopAnimationsModule],
+      providers: [AuthService],
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents()
   })
 
   beforeEach(() => {
     fixture = TestBed.createComponent(UserTableComponent)
     component = fixture.componentInstance
+    component.items$ = of([new User()])
+    Object.assign(component, { skipLoading: true })
     fixture.detectChanges()
   })
 

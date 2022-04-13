adc568d911bb67c8d8d4158b4447375f6964a745
Jadir J. S. Junior <jadirjsjunior@gmail.com>
2022-04-13 13:42:12 -0300

    add modules to manager module

--- src/app/manager/manager.module.ts
+++ src/app/manager/manager.module.ts
@@ -1,11 +1,15 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+
 import { AppMaterialModule } from '../app-material.module'
 import { CommonModule } from '@angular/common'
 import { FlexLayoutModule } from '@angular/flex-layout'
 import { ManagerComponent } from './manager.component'
 import { ManagerHomeComponent } from './manager-home/manager-home.component'
+import { ManagerMaterialModule } from './manager-material.module'
 import { ManagerRoutingModule } from './manager-routing.module'
 import { NgModule } from '@angular/core'
 import { ReceiptLookupComponent } from './receipt-lookup/receipt-lookup.component'
+import { SharedComponentsModule } from '../shared-components.module'
 import { UserManagementComponent } from './user-management/user-management.component'
 import { UserResolve } from '../user/user/user.resolve'
 import { UserTableComponent } from './user-table/user-table.component'
@@ -18,7 +22,16 @@ import { UserTableComponent } from './user-table/user-table.component'
     ReceiptLookupComponent,
     UserTableComponent,
   ],
-  imports: [CommonModule, ManagerRoutingModule, AppMaterialModule, FlexLayoutModule],
+  imports: [
+    CommonModule,
+    ManagerRoutingModule,
+    AppMaterialModule,
+    FlexLayoutModule,
+    SharedComponentsModule,
+    ManagerMaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   providers: [UserResolve],
 })
 export class ManagerModule {}
